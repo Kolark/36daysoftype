700d2b32b871ca9e802160beeae8f37b90eaf80c
Felipe Loaiza Martinez <47009873+Kolark@users.noreply.github.com>
2023-05-17 19:37:53 -0500

    update

--- src/LettersContent/LetterModel.tsx
+++ src/LettersContent/LetterModel.tsx
@@ -28,9 +28,9 @@ type ModelProps = {
 
 export default function Model(props: ModelProps) {
     //Main hooks
-    useGLTF.preload("/gltfCompressed/" + props.model);
+    useGLTF.preload("/36daysoftype/gltfCompressed/" + props.model);
     const group = useRef<THREE.Group>();
-    const { nodes, materials } = useGLTF("/gltfCompressed/" + props.model ) as GLTFResult;
+    const { nodes, materials } = useGLTF("/36daysoftype/gltfCompressed/" + props.model ) as GLTFResult;
     // const { camera, gl } = useThree();
     const myRef = useRef();
 
